4f5850d3c7587bfeff7e228b8a13cccf39120fbc
Lawrence Tsekohol <lawrencetm92@gmail.com>
2022-10-14 17:03:26 -0700

    first commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - print alphabet in lower case
+*Return: Returns 0
+*/
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+	putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
