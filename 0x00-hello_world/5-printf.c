a58651934819329d38df17f78bb1c5d6f072ee64
Lawrence Tsekohol <lawrencetm92@gmail.com>
2022-10-15 14:03:38 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-
 /**
 * main - Hello, printf
 *Return: Returns 0
 */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
+}
