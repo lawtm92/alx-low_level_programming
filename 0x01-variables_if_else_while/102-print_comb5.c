a0a015c78689177168a1828417b725eda7dbc536
Lawrence Tsekohol <lawrencetm92@gmail.com>
2022-10-14 21:10:26 -0700

    first commit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+*main - Software is eating the World
+*Return: Returns 0
+**/
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 99; i++)
+	{
+	for (j = i + 1; j <= 99; j++)
+	{
+	putchar('0' + i / 10);
+	putchar('0' + i % 10);
+	putchar(' ');
+	putchar('0' + j / 10);
+	putchar('0' + j % 10);
+	if (i == 98 && j == 99)
+	break;
+	putchar(',');
+	}
+	}
+	putchar('\n');
+	return (o);
+}
