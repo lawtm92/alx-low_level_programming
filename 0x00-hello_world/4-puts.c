75c67c12fd68c0bb40f186d1fa2d06bb0b2f88ee
Lawrence Tsekohol <lawrencetm92@gmail.com>
2022-10-15 13:59:27 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-*main - Hello, puts
-*Return: Returns 0
+*main - print a line of code using puts
+*Return: 0
 */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle"\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
