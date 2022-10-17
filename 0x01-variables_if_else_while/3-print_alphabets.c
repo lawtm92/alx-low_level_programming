52669a71988c0bc76c11207657a33a80c743d9be
Lawrence Tsekohol <lawrencetm92@gmail.com>
2022-10-17 15:03:10 -0700

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,14 +5,18 @@
 */
 int main()
 {
-	putchar i;
+	char i, j;
 
-	putchar("Capital (upper) and lower case characters:\n");
-	for (i='A'; i<='Z'; i++)
-	putchar("%c ",i);
-	for (i='a'; i<='z'; i++)
-	putchar("%c ",i);
-	return (0);
+	for (i='a'; i <= 'z'; i++)
+	{
+	putchar(i);
+	}
+	for (j='A'; i <= 'Z'; i++)
+	{
+	putchar(j);
+	}
+	putchar('\n');
+	return 0;
 
 }
 
