49c1b7d7be8ee39e2e658aebd277a762fe59f552
Lawrence Tsekohol <lawrencetm92@gmail.com>
2022-10-18 16:38:06 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*main - print alphabets in lower case
+*return: 0
+**/
+void print_alphabet(void);
+{
+	for (letter <= 'a'; letter <= 'z'; letter++)
+	_putchar(letter);
+	_putchar('\n');
+	return (0);
+}
