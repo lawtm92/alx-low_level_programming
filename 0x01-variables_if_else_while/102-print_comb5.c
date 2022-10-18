e1b5b14b43282e62c022d33e8e400f7bbec54e4c
Lawrence Tsekohol <lawrencetm92@gmail.com>
2022-10-17 17:12:08 -0700

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,20 +5,22 @@
 **/
 int main(void)
 {
-	int i, j;
-
-	for (i = 0; i < 99; i++)
+	int digit1, digit2, digit3;
+	
+	for (digit1 = 0; digit1 < 8; digit1++)
 	{
-	for (j = i + 1; j <= 99; j++)
+	for (digit2 = digit1 + 1; digit2 < 9; digit2++)
 	{
-	putchar('0' + i / 10);
-	putchar('0' + i % 10);
-	putchar(' ');
-	putchar('0' + j / 10);
-	putchar('0' + j % 10);
-	if (i == 98 && j == 99)
-	break;
+	for (digit3 = digit2 + 1; digit3 < 10; digit3++)
+	{
+	putchar((digit1 % 10) + '0');
+	putchar((digit2 % 10) + '0');
+	putchar((digit3 % 10) + '0');
+	if (digit1 == 7 && digit2 == 8 && digit3 == 9)
+	continue;
 	putchar(',');
+	putchar(' ');
+	}
 	}
 	}
 	putchar('\n');
