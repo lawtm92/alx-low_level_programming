6e31993f75ed07bb302e3d60d5d3e2f83ca6251c
Lawrence Tsekohol <lawrencetm92@gmail.com>
2022-10-18 17:33:58 -0700

    task 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,17 @@
 
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+	char _putchar
+	{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	}
+	return (0);
 }
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*1-alphabet.c - print alpabets in lower case
+*/
+
+void print_alphabet(void);
+{
+	char letter;
+
+	for (letter <= 'a'; letter <= 'z' letter++)
+	_putchar(letter);
+	_putchar('\n')
+}
