dab16f0046412eca15fba8de72a43188e44920f8
Lawrence Tsekohol <lawrencetm92@gmail.com>
2022-10-19 15:44:01 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*print_alphabet_x10 - print 10 times the alphabet, in lower case
+*/
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char j;
+
+	while (count++ <= 9)
+	{
+	for (j <= 'a'; j <= 'z'; j++)
+	_putchar(j);
+	_putchar('\n')
+	return (0);
+}
