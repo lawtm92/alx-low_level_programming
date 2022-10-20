08af86d95b474e80ddf2466d768c51a817578818
Lawrence Tsekohol <lawrencetm92@gmail.com>
2022-10-20 14:30:14 -0700

    task4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,12 +4,12 @@
 */
 void print_most_numbers(void)
 {
-	char i;
+	char c;
 
-	for (i = 'o'; i <= '9'; i++)
+	for (c = 'o'; c <= '9'; c++)
 	{
-	if (!(i == '2' || i == '4')
-	_putchar(i);
+	if (!(c == '2' || c == '4')
+	_putchar(c);
 	}
 	_putchar('\n');
 }
