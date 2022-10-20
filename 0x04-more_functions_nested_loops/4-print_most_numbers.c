2ec3aee71bdf75bd56715c2c0cb1dd913b3088f4
Lawrence Tsekohol <lawrencetm92@gmail.com>
2022-10-20 13:59:38 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*print_most_numbers - print most numbers
+*/
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 'o'; i <= '9')
+	if (i != '2'; &&i != '4')
+	_putchar(i);
+	_putchar('\n');
+}
