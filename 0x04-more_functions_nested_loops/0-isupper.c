46f8e54aa0e009aa69167897118e569950a07503
Lawrence Tsekohol <lawrencetm92@gmail.com>
2022-10-20 11:50:21 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*_isupper - a function that check for uppercase
+*@c: an input character
+*Return: 1 if c is uppercase or 0 if c is lowercase
+*/
+int _isupper(int c)
+{
+char uppercase = 'A';
+int isupper = 0;
+for (; uppercase <= 'Z'; uppercase++)
+{
+if (c == uppercase)
+{
+ispper = 1;
+break;
+}
+}
+return (isupper);
+}
