e39483a2ef4959ac46dc9c832878a90133dcbe57
Lawrence Tsekohol <lawrencetm92@gmail.com>
2022-10-20 15:35:54 -0700

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* print_diagonal - diagonal line
+*@n: number of times
+*/
+void print_diagonal(int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+_putchar('\');
+}
+_putchar('\n');
+}
