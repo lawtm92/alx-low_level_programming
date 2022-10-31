8db7dc6d63f70acdbce85968689a149732839207
Lawrence Tsekohol <lawrencetm92@gmail.com>
2022-10-31 15:23:21 -0700

    task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,10 +8,9 @@
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
+	unsigned int a;
 
-	for (i = 0; i < n)
-		i++;
-		dest[i] = src[i];
+	for (a = 0; a < n; a++)
+		dest[a] = src[a];
 	return (dest);
 }
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,4 @@
-#ifndef MAIN_H
-#define MAIN_H
+@@ -0,0 +1,10 @@
 int _putchar(char c);
 ar *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -10,4 +9,3 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-#endif
